cf6d7e01b171585b3145219c872f9cd8ddba763b
leechangsin <ycs318@naver.com>
2016-07-22 14:36:06 +0900

    #1
    1. 클래스 생성
      - 로그인 시 이메일과 비밀번호가 일치하는지 확인하기 위한 AuthService클래스 생성

---
+++ src/main/java/service/AuthService.java
@@ -0,0 +1,17 @@
+package service;
+
+import dao.Member;
+
+public class AuthService {
+	
+	private Member member;
+	
+	public void setMember(Member member){
+		this.member = member;
+	}
+	
+	public boolean authenticate(String email, String Password){
+		
+	}
+	
+}
