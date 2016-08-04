dae9ed77138e06e4c2ff09450c4e6a56c753986a
leechangsin <ycs318@naver.com>
2016-08-04 15:44:32 +0900

    #7 DB에 이미지저장을 위한 테스트(Mybatis 연동 시험)   1. pom.xml에 디펜던시 추가   2. Mybatis연동 시험을 위해 MemberDao에 query문 추가   3. Mybatis연동을 위해 spring-Dao설정파일에 빈등록   4. Mybatis로 query문을 날리기위해 query.xml추가
    참고 사이트에서는 Log4j의 로그 기록을 통해 쿼리문의 수행결과를 판단하는데
    나는 아직 Log4j의 사용법을 모르기때문에 Mybatis연동 시험이 성공했다고 가정한다.

--- src/main/java/dao/MemberDao.java
+++ src/main/java/dao/MemberDao.java
@@ -1,5 +1,21 @@
 package dao;
 
-public class MemberDao {
+import java.sql.SQLException;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDao{
+	
+	private SqlSession query;
+	
+	public MemberDao(SqlSession query){
+		this.query = query;
+	}
+	
+	public void testQuery() throws SQLException{
+		query.selectOne("query.test");
+	}
 	
 }
