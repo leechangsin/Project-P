7dd9dce9693b16f6cadcf707bd397b7af9869d8d
leechangsin <ycs318@naver.com>
2016-08-10 12:20:20 +0900

    #6 코드정렬 및 접속DB수정 접속DB는 원래 혁준님이 로컬에서 접속하던 DB에서 내가(창신)접속하는 로컬DB명, 루트 패스워드로 변경

--- src/main/java/dao/MemberDao.java
+++ src/main/java/dao/MemberDao.java
@@ -5,32 +5,27 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import command.Member;
 
-
 public class MemberDao {
 	private JdbcTemplate jdbcTemplate;
 
 	public MemberDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}// end MemberDao(DataSource dataSource)
-	
 
-	
 	public List<Member> selectAll() {
 		String sql = "select * from member";
 
 		List<Member> results = jdbcTemplate.query(sql, new MemberRowMapper());
-		// 만약에query 로 받으면 List 로 받는것으로.. 
+		// 만약에query 로 받으면 List 로 받는것으로..
 		return results;
 	}
-	
+
 	public List<Member> searchByName(String name) {
 		String sql = "select * from member where nickname like concat('%', ? , '%')";
 
 		List<Member> result = jdbcTemplate.query(sql, new MemberRowMapper(), name);
-			
+
 		return result;
-		
+
 	}// end searchByName(String name)
-	
-	
 }
