66fc5205158699ea07c50741c732c4d8fedb3ea8
leechangsin <ycs318@naver.com>
2016-09-14 19:39:13 +0900

    #16 로그인UI - 개발완료 1. LoginUI_Controller   - 회원가입 기능 구현하며 찾아낸 기술 적용

--- src/main/java/controller/LoginUI_Controller.java
+++ src/main/java/controller/LoginUI_Controller.java
@@ -26,7 +26,8 @@ public class LoginUI_Controller {
 		//LoginUI뷰에서 커멘드객체를 사용하여 사용자가 보내는 값을 받고 있고, 커멘드 객체의 프로퍼티에 에러코드를
 		//달아 이를 출력하고 있으므로 커맨드 객체를 생성해서 넘겨주어야한다.
 		//이에 따른 커맨드 객체를 생성하여 넘겨준다.
-		model.addAttribute("memberInfo", new MemberInfo());
+		if(!model.containsAttribute("memberInfo"))
+			model.addAttribute("memberInfo", new MemberInfo());
 		return "LoginUI";
 	}//end login(Model model)
 
@@ -34,7 +35,7 @@ public class LoginUI_Controller {
 	public String loginRequest(MemberInfo memberInfo, Errors errors, Model model) {
 		new MemberInfoValidator().validate(memberInfo, errors);
 		if(errors.hasErrors())
-			return "LoginUI";
+			return showLoginUI(model);
 		
 		String email = memberInfo.getEmail();
 		String passwd = memberInfo.getPasswd();
@@ -42,14 +43,15 @@ public class LoginUI_Controller {
 			LoginRequestService.authenticate(memberInfoDao, email, passwd);
 			//변수명을 Login_Email_Text -> MainUI에서 받는 변수명으로 변경해야함.
 			model.addAttribute("Login_Email_Text", email);
-			return "IndexUI";
 		}catch(NotFindEmailException e){
 			e.printStackTrace();
-			return "LoginUI";
+			return showLoginUI(model);
 		}catch(NotMatchPasswdException e){
 			e.printStackTrace();
-			return "LoginUI";
+			return showLoginUI(model);
 		}//end try
+		
+		return "/Project-P/index";
 	}//end loginRequest(MemberInfo memberInfo, Errors errors)
 	
 	@RequestMapping("goRegistUI")
