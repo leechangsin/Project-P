e05417befd6b2c508dd47c60c05fc9edd34c4b72
leechangsin <ycs318@naver.com>
2016-09-18 20:20:56 +0900

    #메인UI와 로그인UI의 기능연결   1. LoginUI_Controller     - 컨트롤러에서 직접 Dao를 호출하는 방식이 아닌 컨트롤러에서 서비스로 서비스에서 다오로 DB접근 요청하는 방식으로 변경   2. MemberDao     - 로그인을 성공하면 사용자의 계정정보(닉네임, 사진, 자기소개)를 얻어와야 하기 때문에 이메일로 계정정보를 찾는 과정이 필요하다. 그래서 selectByEmail이 추가됨   3. LoginRequestService     - 서비스에서 다오로 디비접근을 요청하기 때문에 memberInfoDao와 memberDao의 의존주입 설정     - selectByEmail : 이메일을 가지고 다오에 검색 요청을 함   4. spring-controller     - 13 ~ 17 : 서비스에서 memberInfoDao와 memberDao 의존주입을 설정했으므로 xml설정에서 이를 주입해줌     - 15 ~ 20 : 컨트롤러에서 loginRequestService 의존주입을 설정했으므로 xml설정에서 이를 주입해줌   5. spring-dao     - 14 : 스키마명을 바꿨으므로 여기서도 스키마 명을 바꿔줌

--- src/main/java/controller/LoginUI_Controller.java
+++ src/main/java/controller/LoginUI_Controller.java
@@ -5,8 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import command.Member;
 import command.MemberInfo;
-import dao.MemberInfoDao;
 import exceptions.NotFindEmailException;
 import exceptions.NotMatchPasswdException;
 import service.LoginRequestService;
@@ -15,10 +15,10 @@ import validator.MemberInfoValidator;
 @Controller
 @RequestMapping("/login")
 public class LoginUI_Controller {
-	private MemberInfoDao memberInfoDao;
+	private LoginRequestService loginRequestService;
 	
-	public LoginUI_Controller(MemberInfoDao memberInfoDao){
-		this.memberInfoDao = memberInfoDao;
+	public LoginUI_Controller(LoginRequestService loginRequestService){
+		this.loginRequestService = loginRequestService;
 	}
 
 	@RequestMapping("main")
@@ -35,27 +35,22 @@ public class LoginUI_Controller {
 	public String loginRequest(MemberInfo memberInfo, Errors errors, Model model) {
 		new MemberInfoValidator().validate(memberInfo, errors);
 		if(errors.hasErrors())
-			return showLoginUI(model);
+			return "LoginUI";
 		
 		String email = memberInfo.getEmail();
 		String passwd = memberInfo.getPasswd();
 		try{
-			LoginRequestService.authenticate(memberInfoDao, email, passwd);
-			//변수명을 Login_Email_Text -> MainUI에서 받는 변수명으로 변경해야함.
-			model.addAttribute("Login_Email_Text", email);
+			loginRequestService.emailAuthenticate(email, passwd);
+			Member member = loginRequestService.selectByEmail(email);
+			model.addAttribute("member", member);
 		}catch(NotFindEmailException e){
 			e.printStackTrace();
-			return showLoginUI(model);
+			return "LoginUI";
 		}catch(NotMatchPasswdException e){
 			e.printStackTrace();
-			return showLoginUI(model);
+			return "LoginUI";
 		}//end try
 		
 		return "/Project-P/index";
 	}//end loginRequest(MemberInfo memberInfo, Errors errors)
-	
-	@RequestMapping("goRegistUI")
-	public String goRegistUI(){
-		return "/Project-P/RegistUI";
-	}
 }//end class LoginUI_Controller
--- src/main/java/service/LoginRequestService.java
+++ src/main/java/service/LoginRequestService.java
@@ -1,13 +1,22 @@
 package service;
 
+import command.Member;
 import command.MemberInfo;
+import dao.MemberDao;
 import dao.MemberInfoDao;
 import exceptions.NotFindEmailException;
 import exceptions.NotMatchPasswdException;
 
 public class LoginRequestService {
+	private MemberInfoDao memberInfoDao;
+	private MemberDao memberDao;
 	
-	public static void authenticate(MemberInfoDao memberInfoDao, String email, String passwd){
+	public LoginRequestService(MemberInfoDao memberInfoDao, MemberDao memberDao){
+		this.memberInfoDao = memberInfoDao;
+		this.memberDao = memberDao;
+	}
+	
+	public void emailAuthenticate(String email, String passwd){
 		MemberInfo memberInfo = memberInfoDao.selectByEmail(email);
 		
 		if(memberInfo == null)
@@ -15,5 +24,10 @@ public class LoginRequestService {
 		else if(!memberInfo.getPasswd().equals(passwd))
 			throw new NotMatchPasswdException("아이디 또는 비밀번호가 일치하지 않습니다.");
 	}//end authenticate(MemberInfoDao memberInfoDao, String email, String password)
+
+	public Member selectByEmail(String email) {
+		// TODO Auto-generated method stub
+		return memberDao.selectByEmail(email);
+	}
 
 }//end class LoginRequestService
