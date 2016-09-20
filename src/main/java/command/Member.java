6739932f5b2fdc99d91875fcf2f9a0bbf7022f64
leechangsin <ycs318@naver.com>
2016-09-20 19:58:16 +0900

    #프로필UI - 개발 중 로그인을 하게되면 로그인컨트롤러에서 로그인 리퀘스트서비스로 로그인 리퀘스트 서비스에서 멤버다오로 사용자가 입력한 이에일을 가지고 Member테이블에서 select * 을 해서 결과를 가져오는데 결과를 담는 member매퍼클래스에서 member클래스의 생성자를 잘못 사용해여 닉네임과 이메일 값이 서로 뒤 바뀌었다 그래서 member클래스의 생성자를 지우고 member매퍼클래스에서 결과를 member클래스에 담을때 setter를 이용하여 담는 방식으로 변경하였다.   1. Member     - 기본생성자, 매게변수가 있는 생성자를 삭제   2. MemberDao     - 불 필요한 임포트문 삭제   3. MemberRowMapper     - Member클래스를 생성할 때 매개변수가 있는 생성자를 사용하지 않고 기본 생성자를 이용하여 생성한다. 값의 할당은 setter를 이용해서 할당한다.

--- src/main/java/command/Member.java
+++ src/main/java/command/Member.java
@@ -6,31 +6,20 @@ public class Member {
 	String intro;
 	Object picture;
 	
-	public Member(){
-		
-	}
-	public Member(String email, String nickname, String intro, String picture) {
-		super();
-		this.email = email;
-		this.nickname = nickname;
-		this.intro = intro;
-		this.picture = picture;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 	public String getNickname() {
 		return nickname;
 	}
 
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getIntro() {
