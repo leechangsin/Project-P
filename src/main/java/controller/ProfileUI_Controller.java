6b2e892fc4fd21cf68dc9e3ddb09ba03ed6119eb
leechangsin <ycs318@naver.com>
2016-10-04 17:45:51 +0900

    #21 프로필 - 프로필   1. ProfileUI_Controller     - 43 ~ 47 : 프로필UI의 메인뷰에서 보여줄 기본정보(이메일,성별,생일,닉네임,자기소개,사진,가입일)을 DB에서 얻어와 모델에 담아 뷰에 전달   2. ProfileService     - 12 ~ 13 : MemberInfoDao의 의존주입 설정     - 18 ~ 19 : 컨트롤러에서 보낸 이메일을 가지고 memberDao에 selectByEmail메서드 호출   3. spring-controller     - MemberInfoDao의 의존주입 스프링 설정

--- src/main/java/controller/ProfileUI_Controller.java
+++ src/main/java/controller/ProfileUI_Controller.java
@@ -40,7 +40,11 @@ public class ProfileUI_Controller {
 	}
 	
 	@RequestMapping("main")
-	public String showProfileUI(){
+	public String showProfileUI(HttpSession session, Model model){
+		Member member = (Member) session.getAttribute("member");
+		MemberInfo memberInfo = profileService.selectByEmail(member.getEmail());
+		model.addAttribute("member", member);
+		model.addAttribute("memberInfo", memberInfo);
 		return "ProfileUI";
 	}
 	
